2a81c851d95940965059d739d48d1e2acc269e13
Kanishkaprabha S. <skanishkaprabha@CEIIND.COM>
2026-02-23 13:57:16 +0530

    pth modified

--- backend/rag/pipeline.py
+++ backend/rag/pipeline.py
@@ -1,5 +1,5 @@
-from rag.retriever import retrieve_movies
-from rag.generator import generate_response
+from backend.rag.retriever import retrieve_movies
+from backend.rag.generator import generate_response
 
 def run_rag(query):
     movies = retrieve_movies(query)
